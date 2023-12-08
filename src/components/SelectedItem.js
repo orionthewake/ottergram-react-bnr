43fe01f6e08edb7a6749b7e6254dba7d03c38b32
Joe Howard <joe.howard@mac.com>
2023-12-08 16:54:26 -0500

    Add SelectedItem component and set up default selected item

---
+++ src/components/SelectedItem.js
@@ -0,0 +1,10 @@
+const SelectedItem = ({ image, name }) => {
+  return (
+    <div className="selected-item">
+      <p>{name}</p>
+      <img src={image} alt={name} />
+    </div>
+  );
+};
+
+export default SelectedItem;
