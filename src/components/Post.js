534cd438a60e36dcdb146987897a17ac58812727
Joe Howard <joe.howard@mac.com>
2023-12-08 17:31:12 -0500

    Add click event handler to Post button

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,11 @@
 const Post = ({ image, name }) => {
+  const handleClick = otterName => {
+    return console.log(`You clicked ${otterName}`);
+  };
+
   return (
     <li className="post-component">
-      <button>
+      <button onClick={() => handleClick(name)}>
         <img src={image} alt={name} />
         <p className="post-name">{name}</p>
       </button>
